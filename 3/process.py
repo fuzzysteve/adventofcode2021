4ae8b138e951514047e0a67140539f86ea1320ab
Steve Anderson / Steve Ronuken <steve@fuzzysteve.co.uk>
2021-12-03 12:46:48 +0000

    Update process.py

--- 3/process.py
+++ 3/process.py
@@ -3,9 +3,10 @@ diag = text_file.readlines()
 diag = map(lambda s: s.strip(), diag)
 text_file.close()
 counter=[0]*12
+limiter=len(diag)/2
 for values in diag:
     digits=list(values)
     counter=[counter[i] + int(digits[i]) for i in range(len(counter))]
 
-gamma=int("".join(map(lambda d: ("1","0")[d<=500] ,counter)),2)
-epsilon=int("".join(map(lambda d: ("1","0")[d>500] ,counter)),2)
+gamma=int("".join(map(lambda d: ("1","0")[d<=limiter] ,counter)),2)
+epsilon=int("".join(map(lambda d: ("1","0")[d>limiter] ,counter)),2)
