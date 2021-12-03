57a9e50c598cea900d2e52488cb77631a47831b9
Steve Anderson / Steve Ronuken <steve@fuzzysteve.co.uk>
2021-12-03 12:27:06 +0000

    Create process.py

---
+++ 3/process.py
@@ -0,0 +1,11 @@
+text_file = open("input", "r")
+diag = text_file.readlines()
+diag = map(lambda s: s.strip(), diag)
+text_file.close()
+counter=[0]*12
+for values in diag:
+    digits=list(values)
+    counter=[counter[i] + int(digits[i]) for i in range(len(counter))]
+
+gamma=int("".join(map(lambda d: ("1","0")[d<=500] ,counter)),2)
+epsilon=int("".join(map(lambda d: ("1","0")[d>500] ,counter)),2)
