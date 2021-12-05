4f868cf473466be10140b0d85270970db716b2b7
Steve Anderson / Steve Ronuken <steve@fuzzysteve.co.uk>
2021-12-05 12:49:04 +0000

    Create process2.py

---
+++ 5/process2.py
@@ -0,0 +1,53 @@
+import re
+text_file = open("input", "r")
+lines = text_file.readlines()
+lines = list(map(lambda s: s.strip(), lines))
+text_file.close()
+
+gridsize=1000
+grid=[]
+
+for gridx in range(0,gridsize):
+    grid.append([0]*gridsize)
+
+
+for line in lines:
+    m=re.search(r'^(\d+),(\d+) -> (\d+),(\d+)$',line)
+    x1=int(m.group(1))
+    y1=int(m.group(2))
+    x2=int(m.group(3))
+    y2=int(m.group(4))
+    xstep=1
+    ystep=1
+    if (y1>y2):
+        ystep=-1
+    if (x1>x2):
+        xstep=-1
+    if x1==x2:
+        start=y1
+        stop=y2
+        if (y1>y2):
+            stop=y1
+            start=y2
+        for ycoord in range(start,stop+1):
+            grid[x1][ycoord]+=1
+    elif y1==y2:
+        start=x1
+        stop=x2
+        if (x1>x2):
+            stop=x1
+            start=x2
+        for xcoord in range(start,stop+1):
+            grid[xcoord][y1]+=1
+    else:
+        #not straight
+        m=float(y2-y1)/float(x2-x1)
+        c=int(y1-(m*x1))
+        for xcoord in range(x1,x2+(1*xstep),xstep):
+            ycoord=int(m*xcoord)+c
+            grid[xcoord][ycoord]+=1
+
+flatten=[item for sublist in grid for item in sublist]
+
+overlap=sum([int(1) for x in flatten if x > 1])
+print(overlap)
